75a29c1216a1ee1888778a99aa8fcc67b1b73f3e
Tobias <sachstobia@aol.com>
2025-10-23 02:38:28 +0200

    Show MAC if in SPI Dump. Show M-ID/M-KEY

--- vanmoof/readSecrets.go
+++ vanmoof/readSecrets.go
@@ -4,15 +4,38 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type Secret struct {
+	Name   string
+	Offset int64
+	Length int
+}
+
 func ReadSecrets(file *os.File) {
-	// Offset in hex and length for the BLE authentication key
-	offset := int64(0x005A000)
-	length := 16 // Number of bytes to read
+	secrets := []Secret{
+		{"BLE Authentication Key", 0x005A000, 16},
+		{"M-ID/M-KEY", 0x005af80, 60},
+		// Add more secrets here as needed
+	}
 
-	buf := readFromFile(file, offset, length)
+	for _, secret := range secrets {
+		buf := readFromFile(file, secret.Offset, secret.Length)
+		if secret.Name == "BLE Authentication Key" {
+			fmt.Printf("%s: %s\n", secret.Name, strings.ToUpper(hex.EncodeToString(buf)))
+		} else {
+			fmt.Printf("%s: %s\n", secret.Name, hex.EncodeToString(buf))
+		}
+	}
 
-	// Convert to hex and print
-	fmt.Println("BLE Authentication Key:", hex.EncodeToString(buf))
+	// Read MAC address with MOOF validation
+	macBuf := readFromFile(file, 0x0005AFE0, 16) // MAC (12) + MOOF (4)
+	macStr := string(macBuf)
+	if strings.HasSuffix(macStr, "MOOF") {
+		mac := macStr[:12] // Extract MAC address part
+		fmt.Printf("MAC Address: %s\n", mac)
+	} else {
+		fmt.Println("MAC Address: MOOF signature not found")
+	}
 }
